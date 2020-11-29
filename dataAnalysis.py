a3d2fc48d2799d684c5b628856b0c10f5e118ea0
alexnesov <alexandrenesovic@gmail.com>
2020-11-29 11:53:50 +0100

    dataAnalysis

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,10 +1,8 @@
 import pandas as pd
-
-len(dfyfinance)
-dfyfinance.dtypes
+import csv
 
 
-class commonStocks():
+class FindCommonStocks():
     """
     Identify common stocks between NYSE eodData list, 
     NASDAQ eodData list and yfinance list
@@ -14,34 +12,54 @@ class commonStocks():
         self.NASDAQList = []
         self.NYSEList = []
 
-    def loadLists():
+        self.commonStocksAll = []
+
+    def loadLists(self):
         """
         yfinanceList = special manipulation because we have to find 
         the distinct values (method "value_counts") among the
         huge dataframe
         """
-        
-        df_yfinance = pd.read_csv('marketdata_2017_01_01_DB.csv')['ticker']
-        df_yfinance.ticker.value_counts().to_frame().reset_index()
-        yfinancelist = test['index'].to_list()
+        df_yfinance = pd.read_csv('Historical/marketdata_2017_01_01_DB.csv')['ticker']
+        yfDistincs = df_yfinance.value_counts().to_frame().reset_index()
+        self.yfinancelist = yfDistincs['index'].to_list()
         self.yfinancelist.sort()
-        self.NASDAQList = pd.read_csv('NASDAQ/NASDAQ_20201118.csv').Symbol.to_list()
-        self.NYSEList = pd.read_csv('NYSE/NYSE_20201120.csv').Symbol.to_list()
+        self.NASDAQList = pd.read_csv('Historical/NASDAQ/NASDAQ_20201118.csv').Symbol.to_list()
+        self.NYSEList = pd.read_csv('Historical/NYSE/NYSE_20201120.csv').Symbol.to_list()
 
-    def 
-        sources = {}
-        sources['yfinance'] = yfinancelist
+    def toSetAndIntersection(self):
 
+        setYfinance = set(self.yfinancelist)
+        setNASDAQ = set(self.NASDAQList)
+        setNYSE = set(self.NYSEList)
 
+        inter_yf_NASDAQ = list(setYfinance.intersection(setNASDAQ))
+        inter_yf_NYSE = list(setYfinance.intersection(setNYSE))
+
+        self.commonStocksAll = inter_yf_NASDAQ + inter_yf_NYSE
+        print(self.commonStocksAll)
+        print(len(self.commonStocksAll))
+        
+    def toCSV(self):
+        with open('Historical/commonStocksAll.csv','w') as csvfile:
+            writer = csv.writer(csvfile)
+            for tick in self.commonStocksAll:
+                writer.writerows([[tick]])
+
+""" 
+        csvwriter = csv.writer(file)
+        csvwriter.writerows([[self.commonStocksAll]])
+ """
 def format():
     newdyfinance = dfyfinance.dropna()
     newdyfinance.to_csv('marketdata_2017_01_01_DB_no_nan.csv', header=False,index=False)
 
 
-
 if __name__ == "__main__":
-    start = commonStocks()
-    start.loadLists
+    start = FindCommonStocks()
+    start.loadLists()
+    start.toSetAndIntersection()
+    start.toCSV()
 
 
 
