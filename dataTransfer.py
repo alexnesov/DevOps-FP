9e62a050e777f90b053df9258e7078997eaf2a72
alexnesov <alexandrenesovic@gmail.com>
2020-11-29 14:01:02 +0100

    dataTransfer

--- dataTransfer.py
+++ dataTransfer.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import csv
+import os
 
+import time
 
-dates = [2015,2016,2017,2018,2019,2020]
+years = [2015,2016,2017,2018,2019,2020]
+months = list(range(1,13)) #-1
 indices = ["NASDAQ", "NYSE"]
 
 
@@ -15,15 +18,25 @@ class batchesToSQL():
     def __init__(self, index):
         self.initialization = True
         self.index = index
-
+        
     def unifyFiles(self):
-        if self.initialization == True:
-            df_init = pd.read_csv('Historical/EODDATA/NASDAQ/NASDAQ_2015/NASDAQ_20150101.csv')
-            df_init.to_csv(f'Historical/EODDATA/{self.index}_Y15.csv', index=False)
-            self.initialization = False
-        else:
-            pass
-            
+
+
+        df_init = pd.read_csv(f'Historical/EODDATA/{self.index}/{self.index}_2015/{self.index}_20150101.csv')
+        df_init.to_csv(f'Historical/EODDATA/{self.index}_Y15.csv', index=False)
+        self.initialization = False
+
+        # the init is done, so we need to remove first element of the list
+        for year in years:
+            print(year)
+            arr = os.listdir(f'Historical/EODDATA/{self.index}/{self.index}_{year}') # change
+            arr.sort()
+            if year == 2015:
+                arr.pop(0)
+            for elem in arr:        
+                df_increment = pd.read_csv(f'Historical/EODDATA/{self.index}/{self.index}_{year}/{elem}')
+                df_increment.to_csv(f'Historical/EODDATA/{self.index}_Y15.csv', mode='a', header=False, index=False)
+        
     def checkDuplicates(self):
         self.NASDAQList = pd.read_csv('Historical/NASDAQ/NASDAQ_20201118.csv').Symbol.to_list()
         self.NYSEList = pd.read_csv('Historical/NYSE/NYSE_20201120.csv').Symbol.to_list()
