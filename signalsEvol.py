104e9ca2d38b8b3bae5d1fb1f432a1ee9fd58a71
alexnesov <alexandrenesovic@gmail.com>
2020-12-30 13:46:07 +0100

    evol

--- signalsEvol.py
+++ signalsEvol.py
@@ -1,25 +1,46 @@
 import pandas as pd
 import numpy as np
 from utils.db_manage import QuRetType, std_db_acc_obj, dfToRDS
+from datetime import datetime, timedelta 
 
 
+today = str(datetime.today().strftime('%Y-%m-%d'))
+yesterday = datetime.today() - timedelta(days=1)
+yesterday = str(yesterday.strftime('%Y-%m-%d'))
 # CREATE TABLE signals.Signals_aroon_crossing (ValidTick VARCHAR(10), SignalDate DATE, ScanDate DATE, NScanDaysInterval INT, PriceAtSignal DECIMAL(5,2), LastClosingPrice DECIMAL(5,2), PriceEvolution DECIMAL(5,2));
 
 
 def signalsPricesEvol():
     """
+    This func not only pulls data but also acts like a data mgmt tool
+    0. Table creation
     1. Pulls signals  from RDS
     2. calculates price evolution
     """
-    qu = "select distinct ValidTick, SignalDate, ScanDate, NScanDaysInterval, PriceAtSignal, `Close` from\
-            (\
-            SELECT Signals_aroon_crossing.*, US_TODAY.Close FROM Signals_aroon_crossing\
-            LEFT JOIN US_TODAY\
-            ON Signals_aroon_crossing.ValidTick = US_TODAY.Symbol\
-            )t\
-            ORDER BY SignalDate DESC"
+    squDelPrevDaysInUSTODAY = f"DELETE FROM marketdata.US_TODAY WHERE Date <'{yesterday}'"
 
-    items = db_acc_obj.exc_query(db_name='marketdata', query=qu, \
+    squDelPreviousTable = "DROP TABLE signals.Signals_aroon_crossing"
+
+    quCreate = "CREATE TABLE signals.Signals_aroon_crossing AS\
+        Select ValidTick, SignalDate, ScanDate, NScanDaysInterval, PriceAtSignal, `Close` FROM\
+        (\
+            SELECT * FROM marketdata.Signals_aroon_crossing\
+            INNER JOIN\
+            (\
+                SELECT Symbol, `Close` \
+                FROM marketdata.US_TODAY\
+                INNER JOIN marketdata.Signals_aroon_crossing\
+                ON Signals_aroon_crossing.ValidTick=US_TODAY.Symbol\
+            )t\
+            ON Signals_aroon_crossing.ValidTick = t.Symbol\
+        )t2;"
+
+    quPull = "SELECT * FROM signals.Signals_aroon_crossing"
+
+    items = db_acc_obj.exc_query(db_name='marketdata', query=squDelPrevDaysInUSTODAY)
+    items = db_acc_obj.exc_query(db_name='marketdata', query=squDelPreviousTable)
+    items = db_acc_obj.exc_query(db_name='marketdata', query=quCreate)
+    items = db_acc_obj.exc_query(db_name='marketdata', query=quPull, \
         retres=QuRetType.ALLASPD)
     
     items['PriceEvolution'] = (( (items.iloc[:,5] - items.iloc[:,4]) / items.iloc[:,4] ) * 100).tolist()
@@ -31,7 +52,6 @@ def signalsPricesEvol():
 if __name__ == '__main__':
     db_acc_obj = std_db_acc_obj() 
     items = signalsPricesEvol()
-    quDeletePreviousDates = f"DELETE FROM signals.Signals_aroon_crossing"
-    db_acc_obj.exc_query(db_name='signals', query=quDeletePreviousDates)
-    dfToRDS(df=items, table='Signals_aroon_crossing', db_name='signals', location='RDS')
+    dfToRDS(df=items, table='Signals_aroon_crossing_evol', db_name='signals', location='RDS')
+    
 
