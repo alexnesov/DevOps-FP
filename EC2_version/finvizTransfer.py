613f8fdec53f8626ac0c75a597496d15ca7238df
alexnesov <alexandrenesovic@gmail.com>
2021-01-03 21:02:39 +0100

    finviz transfer

--- EC2_version/finvizTransfer.py
+++ EC2_version/finvizTransfer.py
@@ -2,7 +2,8 @@ import pandas as pd
 from utils.db_manage import DBManager, QuRetType, dfToRDS, std_db_acc_obj
 import numpy as np
 import os
-
+from datetime import datetime, timedelta 
+import fnmatch
 
 today = str(datetime.today().strftime('%Y-%m-%d'))
 
@@ -10,42 +11,59 @@ today = str(datetime.today().strftime('%Y-%m-%d'))
 
 
 
+df = pd.read_csv(f'~/financials-downloader-bot/downloads/{fileName}')
+
+fileNames = os.listdir('/home/ubuntu/financials-downloader-bot/downloads/Technical')
+TechnicalFiles = []
+for file in fileNames:
+    if fnmatch.fnmatch(file, 'Technical*'):
+        TechnicalFiles.append(file)
 
 
 
-fileName = 'Technical_2020-09-09.csv'
-df = pd.read_csv(f'~/financials-downloader-bot/downloads/{file1}')
+class technicalsToRDS():
 
-def addDate(fileName):
-    date = str(fileName.split('_')[1])
-    date = str(date.split('.csv')[0])
-    df['Date'] = date
-    return df
+    def __init__(self,filename,df):
+        self.df = df
+        self.filename = filename
+
+    def addDateToDF(self):
+        date = str(self.filename.split('_')[1])
+        date = str(date.split('.csv')[0])
+        self.df['Date'] = date
+
+    def renameCols(self):
+        self.df.rename(columns = {'Average True Range':'AverageTrueRange',
+        '20-Day Simple Moving Average':'SimpleMovingAverage20Day',
+        '50-Day Simple Moving Average':'SimpleMovingAverage50Day',
+        '200-Day Simple Moving Average':'SimpleMovingAverage200Day',
+        '52-Week High':'WeekHigh52',
+        '52-Week Low':'WeekLow52',
+        'Relative Strength Index (14)':'RSI14',
+        'Change from Open':'ChangeFromOpen'}, inplace = True) 
+
+    def sendData(self):
+        dfToRDS(df=self.df,table='Technicals',db_name='marketdata')
 
 
-# YYYY-MM-DD
-
-
-df.rename(columns = {'Average True Range':'AverageTrueRange',
-'20-Day Simple Moving Average':'SimpleMovingAverage20Day',
-'50-Day Simple Moving Average':'SimpleMovingAverage50Day',
-'200-Day Simple Moving Average':'SimpleMovingAverage200Day',
-'52-Week High':'WeekHigh52',
-'52-Week Low':'WeekLow52',
-'Relative Strength Index (14)':'RSI14',
-'Change from Open':'ChangeFromOpen'}, inplace = True) 
-
-
+errors = []
 def main():
-    df = addDate(fileName)
-    dfToRDS(df=df,table='Technicals',db_name='marketdata')
-
-
-
+    TechnicalFiles.sort()
+    for file in TechnicalFiles:
+        try:
+            print(file)
+            df = pd.read_csv(f'~/financials-downloader-bot/downloads/{file}')
+            print(df)
+            std = technicalsToRDS(file,df)
+            std.addDateToDF()
+            std.renameCols()
+            std.sendData()
+        except:
+            print("Error for: ", file)
+            errors.append(file)
 
 
 if __name__ == "__main__":
     db_acc_obj = std_db_acc_obj() 
     main()
     
-
