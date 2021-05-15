98921354e88240a348fdf7365a2965589f8cfd87
alexnesov <alexandrenesovic@gmail.com>
2021-05-15 17:09:55 +0200

    jenkins adapt

--- EC2_version/gts.groovy
+++ EC2_version/gts.groovy
@@ -1,3 +1,4 @@
+ 
 pipeline {
     agent any
     stages {
@@ -8,6 +9,26 @@ pipeline {
                     . /var/lib/jenkins/virtualenvs/test-env/bin/activate
                     cd /home/ubuntu/Signal-Detection
                     . /home/ubuntu/.bashrc; python -u /home/ubuntu/Signal-Detection/Signal_detection.py > /home/ubuntu/Signal-Detection/utils/Signal_detection.log 2>&1
+                '''
+        }
+    }
+        stage('Signal_detection_evol') {
+            steps {
+                sh '''
+                    #!/bin/bash
+                    . /var/lib/jenkins/virtualenvs/test-env/bin/activate
+                    cd /home/ubuntu/Signal-Detection
+                    . /home/ubuntu/.bashrc; python -u /home/ubuntu/Signal-Detection/signalsEvol.py > /home/ubuntu/Signal-Detection/utils/signalsEvol.log 2>&1
+                '''
+            }
+        }
+        stage('Detailed_generation') {
+            steps {
+                sh '''
+                    #!/bin/bash
+                    . /var/lib/jenkins/virtualenvs/test-env/bin/activate
+                    cd /home/ubuntu/Signal-Detection
+                    . /home/ubuntu/.bashrc; python -u /home/ubuntu/Signal-Detection/DetailedGeneration.py > /home/ubuntu/Signal-Detection/utils/DetailedGeneration.log 2>&1
                 '''
         }
     }
