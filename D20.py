de9bf6c4d17d371d32b6212a8e0c854edda2a43c
alexnesov <alexandrenesovic@gmail.com>
2021-06-08 22:33:15 +0200

    D20

--- D20.py
+++ D20.py
@@ -33,36 +33,39 @@ colNames = {0:"ValidTick",
             9:"Industry"}
 
 dfitems = dfitems.rename(columns=colNames)
-
-
-
 PriceEvolution = dfitems['PriceEvolution'].tolist()
 
 # Calculate nbSignals
 nSignalsDF = dfitems[['ValidTick','SignalDate']]
-
-    
 items = db_acc_obj.exc_query(db_name='signals', query=qu, \
 retres=QuRetType.ALL)
 
-print('DF: ')
-print(dfitems.columns)
-dfnew = dfitems
-dfnew['D20'] = dfnew['SignalDate'] + timedelta(days=20)
 
-print(dfnew[['ValidTick','SignalDate','LastClosingPrice','D20']])
+# Creating a column that contains the dateof the signal +20days, for every stock
+dfSignals = dfitems
+dfSignals['D20'] = dfSignals['SignalDate'] + timedelta(days=20)
+print(dfSignals[['ValidTick','SignalDate','LastClosingPrice','D20']])
 
 
-tupNew = tuple(dfnew['ValidTick'])
 
+# Getting the price for every stock, at d+20
+tupNew = tuple(dfSignals['ValidTick'])
 QUpricesNASDAQ = f"select * from marketdata.NASDAQ_20 where Symbol IN {tupNew} and Date>'2020-12-15' \
     and Date<'{todayD20}'"
 QUpricesNYSE = f"select * from marketdata.NYSE_20 where Symbol IN {tupNew} and Date>'2020-12-15'\
     and Date<'{todayD20}'"
 
-test = db_acc_obj.exc_query(db_name='marketdata', query=QUpricesNYSE, \
+NASDAQPrices = db_acc_obj.exc_query(db_name='marketdata', query=QUpricesNYSE, \
     retres=QuRetType.ALLASPD)
-test['Date'] = test['Date'].astype(str)
+
+
+NYSEPrices = db_acc_obj.exc_query(db_name='marketdata', query=QUpricesNYSE, \
+    retres=QuRetType.ALLASPD)
+
+
+# Concatenate both dataframe to have one single consolidated
+allPrices = pd.concat([NASDAQPrices,NYSEPrices])
+allPrices['Date'] = allPrices['Date'].astype(str)
 
 
 
@@ -72,8 +75,10 @@ def getPrice(symbol, date):
     param date: str, yyyy-mm-dd
     returns price: int
     """
-    price = float(test.loc[(test['Symbol']==symbol) & (test['Date']==date)]['Close']) 
+    price = float(allPrices.loc[(allPrices['Symbol']==symbol) & (allPrices['Date']==date)]['Close']) 
     return price
 
 
 getPrice('AAIC','2020-12-16')
+
+
