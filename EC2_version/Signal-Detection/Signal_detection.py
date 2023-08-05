b665dea346e4d1073fcd2f59cec8a5258a6f3388
alexnesov <alexandrenesovic@gmail.com>
2023-08-05 20:13:52 +0200

    Signal detection working

--- EC2_version/Signal-Detection/Signal_detection.py
+++ EC2_version/Signal-Detection/Signal_detection.py
@@ -1,15 +1,13 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None 
-import yfinance as yf
-from talib import MA_Type
-import talib
 import numpy as np
 from datetime import datetime, timedelta 
-from time import gmtime, strftime
+from time import strftime
 import os
 from utils.db_manage import QuRetType, dfToRDS, std_db_acc_obj
-import sys
-sys.stdout.flush()
+from ta import aroon, rsi
+pd.set_option('display.max_rows', 80)
+pd.set_option('display.max_columns', 20)
 
 PATH_SIG_DETECTION = "/home/ubuntu/Signal-Detection"
 
@@ -18,18 +16,21 @@ today = datetime.today()
 today_str = today.strftime('%Y-%m-%d')
 now = strftime("%H:%M:%S")
 now = now.replace(":","-")
-currentDirectory = os.getcwd() # Ubuntu
+currentDirectory = os.getcwd() 
 
 # PARAMETERS
-Aroonval = 40
-short_window =10
-long_window = 50
-NDaysValidationPeriod = 90
-ValidPeriodStart = (today - timedelta(days=NDaysValidationPeriod)).strftime('%Y-%m-%d')
+Aroonval                    = 40
+short_window                = 10
+long_window                 = 50
+timePeriodRSI               = 14
+NDaysValidationPeriod       = 90
+ValidPeriodStart            = (today - timedelta(days=NDaysValidationPeriod)).strftime('%Y-%m-%d')
+
+
 # start_date and end_date are used to set the time interval that in which a signal is going to be searched
-NScanDaysInterval = 2
-start_date = today - timedelta(days=NScanDaysInterval)
-end_date = f'{today}'
+NScanDaysInterval           = 5
+START_DATE                  = today - timedelta(days=NScanDaysInterval)
+END_DATE                    = f'{today}'
 
 
 # file that is going to contain valid symbols
@@ -43,7 +44,7 @@ for k in keys:
     validSymbols[k] = []
 
 
-def SignalDetection(df, tick, *args):
+def SignalDetection(df: pd.DataFrame) -> pd.DataFrame:
     """
     This function downloads prices for desired quotes (those in the parameter)
     and then tries to catch signals for selected timeframe.
@@ -52,78 +53,81 @@ def SignalDetection(df, tick, *args):
     :param p2: ticker
     :returns: df with signals
     """
+    try:
+        # Aroon
+        aroonUP, aroonDOWN = aroon(df, Aroonval)
+        
+        # RSI
+        ind_rsi             = rsi(df, timePeriodRSI)
+        df['RSI']           = ind_rsi
+        df['Aroon Down']    = aroonDOWN
+        df['Aroon Up']      = aroonUP
 
-    close = df["Close"].to_numpy()
-    high = df["High"].to_numpy()
-    low = df["Low"].to_numpy()
+        new_series_signal   = pd.Series(np.zeros(len(df)), index=df.index)
+        new_series_aroon    = pd.Series(np.zeros(len(df)), index=df.index)
 
-    # Aroon
-    aroonDOWN, aroonUP = talib.AROON(high=high, low=low,timeperiod=Aroonval)  ####
-    # RSI
-    real = talib.RSI(close, timeperiod=14)
+        df['signal']        = new_series_signal
+        df['signal_aroon']  = new_series_aroon
+        df                  = df.reset_index()
+        # Moving averages
+        df['short_mavg']    = df['Close'].rolling(window=short_window, min_periods=1, center=False).mean()
+        df['long_mavg']     = df['Close'].rolling(window=long_window, min_periods=1, center=False).mean()
 
-    df['RSI'] = real
-    df['Aroon Down'] = aroonDOWN
-    df['Aroon Up'] = aroonUP
-    df['signal'] = pd.Series(np.zeros(len(df)))
-    df['signal_aroon'] = pd.Series(np.zeros(len(df)))
-    df = df.reset_index()
-    
-    # Moving averages
-    df['short_mavg'] = df['Close'].rolling(window=short_window, min_periods=1, center=False).mean()
-    df['long_mavg'] = df['Close'].rolling(window=long_window, min_periods=1, center=False).mean()
+        # When 'Aroon Up' crosses 'Aroon Down' from below
 
-    # When 'Aroon Up' crosses 'Aroon Down' from below
-    df["signal"][short_window:] =np.where(df['short_mavg'][short_window:] > df['long_mavg'][short_window:], 1,0)
-    df["signal_aroon"][short_window:] =np.where(df['Aroon Down'][short_window:] < df['Aroon Up'][short_window:], 1,0)
+        df["signal"][short_window:]         = np.where(df['short_mavg'][short_window:] > df['long_mavg'][short_window:], 1,0)
+        df["signal_aroon"][short_window:]   = np.where(df['Aroon Down'][short_window:] < df['Aroon Up'][short_window:], 1,0)
 
-    df['positions'] = df['signal'].diff()
-    df['positions_aroon'] = df['signal_aroon'].diff()
-    df['positions_aroon'].value_counts()
+        df['positions']                     = df['signal'].diff()
+        df['positions_aroon']               = df['signal_aroon'].diff()
+        df['positions_aroon'].value_counts()
 
-    # Trend reversal detection
-    # Aroon alone doesn't give us enough information.
-    # Too many false signals are given
-    # We blend moving averages crossing strategy
-    df['doubleSignal'] = np.where(
-        (df["Aroon Up"] > df["Aroon Down"]) & (df['positions']==1) & (df["Aroon Down"]<75) &(df["Aroon Up"]>55),
-        1,0)
-    df['symbol'] = tick
+        # Trend reversal detection
+        # Aroon alone doesn't give us enough information.
+        # Too many false signals are given
+        # We blend moving averages crossing strategy
+        df['doubleSignal'] = np.where(
+            (df["Aroon Up"] > df["Aroon Down"]) & (df['positions']==1) & (df["Aroon Down"]<75) &(df["Aroon Up"]>55),
+            1,0)
+        
+    except ValueError as e:
+        columns = ["index", "Symbol", "Date", "Open", "High", "Low", "Close", "Volume", "RSI", "Aroon Down", "Aroon Up", "signal", "signal_aroon", "short_mavg", "long_mavg", "positions", "positions_aroon", "doubleSignal"]
+
+        # Create an empty DataFrame with the specified columns
+        empty_df = pd.DataFrame(columns=columns)
+        
+        return empty_df
 
     return df
 
 
-def lastSignalsDetection(signals_df, tick, start_date, end_date):
+def lastSignalsDetection(signals_df: pd.DataFrame, tick: str):
     """
     param: df (one tick) with all signals already detected
-    Checking if signal is present in the last x days (start_date & end_date)
+    Checking if signal is present in the last x days (START_DATE & END_DATE)
     Func doesn't return anything, it appends selected stock for given time interval in empty list
     (list = validsymbol)
     """
-    DFfinalsignal = signals_df[['Date','Close','doubleSignal']]
-    DFfinalsignal['Date'] = pd.to_datetime(DFfinalsignal['Date'], format='%Y-%m-%d')
-    mask = (DFfinalsignal['Date'] > start_date) & (DFfinalsignal['Date'] <= end_date)
-    DFfinalsignal = DFfinalsignal.loc[mask]
-    true_false = list(DFfinalsignal['doubleSignal'].isin(["1"]))
+    DFfinalsignal               = signals_df[['Date','Close','doubleSignal']]
+    DFfinalsignal['Date']       = pd.to_datetime(DFfinalsignal['Date'], format='%Y-%m-%d')
+    mask                        = (DFfinalsignal['Date'] > START_DATE) & (DFfinalsignal['Date'] <= END_DATE)
+    DFfinalsignal               = DFfinalsignal.loc[mask]
+    signaled                    = (DFfinalsignal['doubleSignal'] == 1).any()
 
     # Append the selected symbols to empty initialized list "validsymbol"
-    if True in true_false:
-        lastSignalDF = DFfinalsignal.loc[DFfinalsignal['doubleSignal']==1]
-        lastSignalPrice = list(lastSignalDF.loc[-1:,'Close'])[0]
-        string_lastSignalDate = list(lastSignalDF.loc[-1:,'Date'])[0].strftime("%Y-%m-%d") 
+    if signaled:
+        lastSignalDF            = DFfinalsignal.loc[DFfinalsignal['doubleSignal']==1]
+        lastSignalPrice         = list(lastSignalDF.loc[-1:,'Close'])[0]
+        string_lastSignalDate   = list(lastSignalDF.loc[-1:,'Date'])[0].strftime("%Y-%m-%d") 
         
         validSymbols['ValidTick'].append(tick) 
         validSymbols['SignalDate'].append(string_lastSignalDate)
         validSymbols['ScanDate'].append(today_str)
         validSymbols['NScanDaysInterval'].append(NScanDaysInterval)
         validSymbols['PriceAtSignal'].append(lastSignalPrice)
-        
         print(f'Signal detected for {tick}.')
     else:
         pass
-
-    # re-initilization
-    true_false = False
 
 
 quCopy = "CREATE TABLE Signals_aroon_crossing_copy AS (\
@@ -147,33 +151,33 @@ def csvToRDS():
     db_acc_obj.exc_query(db_name='signals', query=quRenameTable)
 
 def main():
-    INIT = True
 
     stockexchanges = ['NASDAQ','NYSE']
 
     remainingNStock = 0
     for SE in stockexchanges:
-        lenSE = len(pd.read_csv(f'{PATH_SIG_DETECTION}/utils/{SE}_list.csv'))
+        try:
+            lenSE = len(pd.read_csv(f'{PATH_SIG_DETECTION}/utils/{SE}_list.csv'))
+        except FileNotFoundError:
+            lenSE = len(pd.read_csv(f'{currentDirectory}/utils/{SE}_list.csv'))
+
         remainingNStock += lenSE
     batch500 = remainingNStock - 500
 
     for SE in stockexchanges:
-        dftickers = pd.read_csv(f'{PATH_SIG_DETECTION}/utils/{SE}_list.csv')
+        try:
+            dftickers = pd.read_csv(f'{PATH_SIG_DETECTION}/utils/{SE}_list.csv')
+        except FileNotFoundError:
+            dftickers = pd.read_csv(f'{currentDirectory}/utils/{SE}_list.csv')
+
         tickers = dftickers["Symbol"].tolist()
 
-        print()
-        print("tickers: ")
-        print(tickers)
-
-        qu = f"SELECT * FROM {SE}_15 WHERE DATE > '{ValidPeriodStart}'"
-        print(f'Querying data for : {SE}. . .')
+        qu = f"SELECT * FROM {SE}_20 WHERE DATE > '{ValidPeriodStart}'"
+        print(f':INFO: Querying data for : {SE}. . .')
         initialDF = db_acc_obj.exc_query(db_name='marketdata', query=qu, \
         retres=QuRetType.ALLASPD)
-        print('Starting TA. . .')
 
-        if INIT:
-            initialDF.to_csv("test.csv")
-            INIT = False
+        print(':INFO: Starting Technical Analysis. . .')
 
         for tick in tickers:
             tick = str(tick)
@@ -182,24 +186,27 @@ def main():
 
             remainingNStock -= 1
             if remainingNStock==batch500:
-                print(f'{remainingNStock} stocks remaining.')
+                print(f':INFO: {remainingNStock} stocks remaining.')
                 batch500 = remainingNStock - 500
-            try:
-                dfTick = initialDF.loc[initialDF['Symbol']==f'{tick}']
-                if dfTick.empty:
-                    continue
-                df = SignalDetection(dfTick,tick)
-                lastSignalsDetection(df, tick, start_date, end_date)
-            except:
-                pass
+
+            dfTick = initialDF.loc[initialDF['Symbol']==f'{tick}']
+            if dfTick.empty:
+                continue
+
+            df = SignalDetection(dfTick)
+            lastSignalsDetection(df, tick)
+
 
         tocsvDF = pd.DataFrame.from_dict(validSymbols)
-        tocsvDF.to_csv(f'{PATH_SIG_DETECTION}/utils/batch_{today_str}.csv')
-
-    print('TA successfully accomplished.')
-    print('Sending data to RDS. . .')
-    csvToRDS()
-    print('Completed.')
+        try:
+            tocsvDF.to_csv(f'{PATH_SIG_DETECTION}/utils/batch_{today_str}.csv')
+        except (FileNotFoundError, OSError):
+            tocsvDF.to_csv(f'{currentDirectory}/utils/batch_{today_str}.csv')
+    
+    print(':INFO: Technical Analysis successfully accomplished.')
+    # print(':INFO: Sending data to RDS. . .')
+    # csvToRDS()
+    # print(':INFO: Completed.')
 
 
 
---
+++ EC2_version/Signal-Detection/ta.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+def aroon(data: pd.DataFrame, lb: int = 25) -> tuple:
+    """
+    Calculates the Aroon Up and Aroon Down indicators for a given DataFrame.
+    :param data: DataFrame containing 'High' and 'Low' prices.
+    :param lb: Lookback period for calculating Aroon (default: 25).
+    :return: A tuple of Aroon Up and Aroon Down lists.
+    """
+    df = data.copy()
+    df['up'] = 100 * df.High.rolling(lb + 1).apply(lambda x: x.argmax()) / lb
+    df['dn'] = 100 * df.Low.rolling(lb + 1).apply(lambda x: x.argmin()) / lb
+
+    return df['up'].tolist(), df['dn'].tolist() 
+
+
+def rsi(df: pd.DataFrame, timePeriodRSI: int) -> pd.Series:
+    """
+    Calculates the Relative Strength Index (RSI) for a given DataFrame.
+    :param df: DataFrame containing 'Close' prices.
+    :param timePeriodRSI: Time period for calculating RSI.
+    :return: Series containing the RSI values.
+    """
+
+    close = df['Close']
+    # Get the difference in price from previous step
+    delta = close.diff()
+    # Get rid of the first row, which is NaN since it did not have a previous 
+    # row to calculate the differences
+    delta = delta[1:] 
+
+    # Make the positive gains (up) and negative gains (down) Series
+    up, down = delta.clip(lower=0), delta.clip(upper=0)
+
+    # Calculate the EWMA
+    roll_up1 = up.ewm(span=timePeriodRSI).mean()
+    roll_down1 = down.abs().ewm(span=timePeriodRSI).mean()
+
+    # Calculate the RSI based on EWMA
+    RS1 = roll_up1 / roll_down1
+    RSI1 = 100.0 - (100.0 / (1.0 + RS1))
+
+    # Calculate the SMA
+    roll_up2 = up.rolling(timePeriodRSI).mean()
+    roll_down2 = down.abs().rolling(timePeriodRSI).mean()
+
+    # Calculate the RSI based on SMA
+    RS2 = roll_up2 / roll_down2
+    RSI2 = 100.0 - (100.0 / (1.0 + RS2))
+
+    return RSI2
+
+
